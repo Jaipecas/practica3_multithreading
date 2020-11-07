350111fc0b28faa29a61c9653a45006a40fd6e34
Jaime Perez <jaipecas89@gmail.com>
2020-11-07 20:23:31 +0100

    Ejercicio 1

---
+++ Ejercicio1.py
@@ -0,0 +1,27 @@
+import threading
+import time
+
+
+class myThread(threading.Thread):
+    def __init__(self, name):
+        threading.Thread.__init__(self)
+        self.name = name
+
+    def run(self):
+        print("Lanzando hilo: " + self.name)
+        time.sleep(3)
+        print("%s: %s" % (self.name, time.ctime(time.time())))
+
+
+if __name__ == '__main__':
+    threads = []
+
+    for i in range(21):
+        thread = myThread(i)
+        thread.start()
+        threads.append(thread)
+
+    for t in threads:
+     t.join()
+
+print("Exiting Main Thread")
